8fba1b1a8285f3701079d083d544d8263652299d
Smiley-K777 <48253244+Smiley-K777@users.noreply.github.com>
2021-06-23 21:45:04 +0200

    Add files via upload

---
+++ Random.bat
@@ -0,0 +1,11 @@
+@echo off
+Pause
+
+start Random.bat
+
+:k
+color 14
+echo %Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%
+color 41
+echo %Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%	%Random%
+goto k
